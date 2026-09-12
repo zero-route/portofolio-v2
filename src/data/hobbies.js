a24904c019bf11c3550981565413fa45acda24bb
zero-route <iostream911@gmail.com>
2026-09-12 19:55:45 +0800

    update hobbies.js

--- src/data/hobbies.js
+++ src/data/hobbies.js
@@ -1,5 +1,5 @@
 export const hobbies = [
-  { id: 1, src: "/images/hobby+1.jpg", caption: "Jalan-jalan" },
+  { id: 1, src: "/images/hobby-1.jpg", caption: "Jalan-jalan" },
   { id: 2, src: "/images/hobby-2.jpg", caption: "Game" },
   { id: 3, src: "/images/hobby-3.jpg", caption: "Kopi Time" },
   { id: 4, src: "/images/hobby-4.jpg", caption: "Coding" },
