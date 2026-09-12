a1602d5eca9628aea9e7737f5369860ea8d6d45f
zero-route <iostream911@gmail.com>
2026-09-12 19:11:21 +0800

    update hobbies.js add caption

--- src/data/hobbies.js
+++ src/data/hobbies.js
@@ -1,8 +1,8 @@
 export const hobbies = [
   { id: 1, src: "/images/hobby/1.jpg", caption: "Jalan-jalan" },
-  { id: 2, src: "/images/hobby/2.jpg", caption: "" },
-  { id: 3, src: "/images/hobby/3.jpg", caption: "" },
-  { id: 4, src: "/images/hobby/4.jpg", caption: "" },
-  { id: 5, src: "/images/hobby/5.jpg", caption: "" },
-  { id: 6, src: "/images/hobby/6.jpg", caption: "" },
+  { id: 2, src: "/images/hobby/2.jpg", caption: "Game" },
+  { id: 3, src: "/images/hobby/3.jpg", caption: "Kopi Time" },
+  { id: 4, src: "/images/hobby/4.jpg", caption: "Coding" },
+  { id: 5, src: "/images/hobby/5.jpg", caption: "Nonton" },
+  { id: 6, src: "/images/hobby/6.jpg", caption: "Network" },
 ];
